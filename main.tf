708ad956d24ea6fea27d0bc78e668397160ff0fa
sarveswarreddy81 <sarveswarreddykalvapalli@gmail.com>
2025-10-16 17:12:48 +0530

    updated main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "myvpc" {
 resource "aws_subnet" "subnet1" {
   vpc_id = aws_vpc.myvpc.id
   cidr_block = "10.0.0.0/24"
-  availability_zone = "eu-north-1a"
+  availability_zone = "us-east-1a"
   tags = {
     Name="web"
   }
