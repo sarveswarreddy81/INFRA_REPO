11c0665d78e93a38efa9b4ee987c7acfa30c0c2e
sarveswarreddy81 <sarveswarreddykalvapalli@gmail.com>
2025-10-16 16:49:34 +0530

    infra-2

--- provider.tf
+++ provider.tf
@@ -1,11 +1,14 @@
 terraform {
+  required_version = ">= 1.0.0"
+
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "3.1.0"
+      source  = "hashicorp/aws"
+      version = "~> 3.1.0"
     }
   }
 }
+
 provider "aws" {
-    region ="eu-north-1"
+  region = "us-east-1"
 }
